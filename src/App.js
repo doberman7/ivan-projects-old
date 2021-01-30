79466fd73787f6082d61d421f3e659f461a7d0ca
ivan <ivanrubioangeles@gmail.com>
2021-01-30 12:36:23 -0600

    particles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import React from "react";
 import Home from "./components/Home";
+import Particles from "react-particles-js";
+import particlesConfig from "./particlesConfig";
 
 const App = () => {
   return (
-    <div className="App">
+    <div className="App" style={{ position: "relative", overflow: "hidden" }}>
+      <div style={{ position: "absolute" }}>
+        <Particles height="100vh" width="100vw" params={particlesConfig} />
+      </div>
       <Home />
     </div>
   );
