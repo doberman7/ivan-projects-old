e1efbd46d637a4303721defa973010ff31411f54
ivan <ivanrubioangeles@gmail.com>
2021-01-30 12:12:11 -0600

    dont breack

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import "./App.css";
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import LayoutApp from "./components/Layout";
 import Home from "./components/Home";
 
 const App = () => {
   return (
     <div className="App">
-      <BrowserRouter>
-        <LayoutApp>
-          <Switch>
-            <Route path="/" component={Home} exact />
-          </Switch>
-        </LayoutApp>
-      </BrowserRouter>
+      <Home />
     </div>
   );
 };
